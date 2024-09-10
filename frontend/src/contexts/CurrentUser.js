e984958dfe2a02249acff227caebb7869951047b
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-09-09 22:00:32 -0400

    Fix Error on Front End CurrentUser

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -6,18 +6,7 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
-    useEffect(() => {
-
-        const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile',{
-                credentials: 'include'
-            })
-            let user = await response.json()
-            setCurrentUser(user)
-        }
-        getLoggedInUser()
-    }, [])
-  
+   
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
